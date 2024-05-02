82e01827136c49d3b10e4d4892b3073c33b4acef
Kassem2000 <144707158+Kassem2000@users.noreply.github.com>
2024-05-02 22:07:19 +0200

    Headern is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,18 @@ const Header = () => {
     
     <header className="header">
       <div className="SpeedAuction-logo">
+        <a href="/">
         <img src="/SpeedAuction.png" alt="SpeedAuction" className="left-logo" />
+        </a>
       </div>
       <nav>
         <ul className="links">
+          <a href="/login">
           <li>Login</li>
+          </a>
+          <a href="/signup">
           <li>Sign up</li>
+          </a>
         </ul>
       </nav>
       <div className="Profile-log">
