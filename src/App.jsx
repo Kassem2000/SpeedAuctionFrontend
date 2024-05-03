2c5a957fd58a8cac73c0fe82954195aba32aefbe
Kassem2000 <144707158+Kassem2000@users.noreply.github.com>
2024-05-03 09:21:39 +0200

    Header is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import SignupPage from "./pages/SignupPage"
-import CreateAuctionPage from "./pages/CreateAuctionPage"
-import ProfilePage from "./pages/ProfilePage"
-import BidPage from "./pages/BidPage"
-import Header from "./components/Header"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import CreateAuctionPage from "./pages/CreateAuctionPage";
+import ProfilePage from "./pages/ProfilePage";
+import BidPage from "./pages/BidPage";
+import Header from "./components/Header";
 
 function App() {
-  
-
   return (
-      <BrowserRouter>
-       <div>
-        <Header />
+    <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/bid" element={<BidPage />}/>
-        <Route path="/createAuction" element={<CreateAuctionPage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/signup" element={<SignupPage />}/>
-        <Route path="/profile" element={<ProfilePage />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/bid" element={<BidPage />} />
+        <Route path="/createAuction" element={<CreateAuctionPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
-      </div>
-      </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
