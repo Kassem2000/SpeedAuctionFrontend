3a4b3c293e45ab40e177ccb867e492f4aa27652c
abocis <aboucisse22@gmail.com>
2024-05-04 19:07:08 +0200

    register ui ready with respetive field need to add a link to home page that could take me to sign up.. all styling are located in the index.css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
-      Home
+      <h1>Welcome to the Home Page</h1>
+      <Link to="/signup">Signup</Link>
+      
     </div>
   )
 }
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,10 +1,52 @@
+import React, { useState } from 'react'
 
 
 const SignupPage = () => {
   return (
-    <div>
-      signup
+    <div className="register-form">
+      <form>
+        <h1>Sign up</h1>
+        <div className="input-group">
+          <label>
+            <input type="text" name="Username" placeholder='Usename'/>
+          </label>
+          <label>
+            <input type="text" name="Firstname" placeholder='FirstName'/>
+          </label>
+          <label>
+            <input type="text" name="lastname"placeholder='LastNAme' />
+          </label>
+          <label>
+            <input type='text' name="phone number" placeholder='Phone'/>
+          </label>
+          <label>
+            
+            <input type="text" name="email" placeholder='Email'/>
+          </label>
+          
+        </div>
+        <div className="input-group">
+        <label>
+            <input type="text" name="Adress" placeholder='Address'/>
+          </label>
+          <label>
+            <input type="text" name="Country" placeholder='Country'/>
+          </label>
+          <label>
+            
+            <input type="text" name="City" placeholder='City'/>
+          </label>
+          <label>
+            <input type='text' name="postal code" placeholder='Postal Code'/>
+          </label>
+          <label>
+            <input type='text' name="postal code" placeholder='Postal Code'/>
+          </label>
+        </div>
+        <button type="submit">Confirm</button>
+      </form>
     </div>
+  
   )
 }
 
