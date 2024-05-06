1a6fe38737fbbd7fc528e4d0f9ec884d07c93f6f
andronisdre <andrebulow13@gmail.com>
2024-05-06 22:37:12 +0200

    added filter and createAuction button over the hero image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,24 @@ import HeroImage from "../components/HeroImage";
 const HomePage = () => {
   return (
     <>
-      <HeroImage />
+      <div className="heroHome">
+        <div className="createAuctionButton">
+          <h3>Create Auction</h3>
+        </div>
+        <div className="filterWrapper">
+          <div className="filterBy">
+            <h3>Year</h3>
+            <h3>Model</h3>
+            <h3>Brand</h3>
+            <h3>Miles Driven</h3>
+            <h3>Color</h3>
+            <h3>Price</h3>
+          </div>
+          <div className="filterButton">
+            <h3>Filter Auctions</h3>
+          </div>
+        </div>
+      </div>
       <div className="thumbnailWrapper">
         <Thumbnail />
         <Thumbnail />
