ce3fcc96e3ce77dda255c3002512530a93ab5c80
Kassem2000 <144707158+Kassem2000@users.noreply.github.com>
2024-05-21 08:48:19 +0200

    Updated

--- src/context/AuctionContext.jsx
+++ src/context/AuctionContext.jsx
@@ -29,16 +29,12 @@ const AuctionProvider = ({ children }) => {
         `${import.meta.env.VITE_API_URL}/bids/filterByAuctionId/${auctionId}`
       );
 
-      
-
       const auctionData = {
         ...resAuctions.data,
         topBid: TopBid.data.amount,
         bidCount: TotalBids.data.length,
         _id: auctionId,
       };
-
-      
 
       setAuction(AuctionCar.data[0]);
       setDisplayedAuction(auctionData);
