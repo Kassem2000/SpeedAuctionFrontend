cb21e21359d57cbe9fd22d184aa255e96f074149
Kassem2000 <144707158+Kassem2000@users.noreply.github.com>
2024-05-21 16:13:56 +0200

    Connect BidPage With Backend, everything is done

--- src/context/AuctionContext.jsx
+++ src/context/AuctionContext.jsx
@@ -42,7 +42,6 @@ const AuctionProvider = ({ children }) => {
       console.error("error: " + err);
     }
   };
-  
 
   const addBid = async (auctionId, bidAmount) => {
     const user = window.localStorage.getItem("user");
